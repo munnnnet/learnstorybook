8b381fffaa6f520ca240818234dd2c95ade60d94
Fathul Mundzir <akun.mundzir@gmail.com>
2020-07-25 20:41:43 +0700

    Update Guides.js
    Change from:
    
    @media (min-width: ${breakpoint}px) {
          max-width: calc(50% - 50px);
          margin: 25px;
        }
    
    Become:
    
    @media (min-width: ${breakpoint}px) {
          max-width: calc(50% - 50px);
          margin: 10px;
        }

--- src/components/screens/IndexScreen/Guides.js
+++ src/components/screens/IndexScreen/Guides.js
@@ -27,7 +27,7 @@ const Content = styled.div`
 
     @media (min-width: ${breakpoint}px) {
       max-width: calc(50% - 50px);
-      margin: 25px;
+      margin: 10px;
     }
 
     @media (min-width: ${breakpoint * 1.75}px) {
