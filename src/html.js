4674c01aa8f798e5c3073ab90f18a5ac382f8a42
Fathul Mundzir <akun.mundzir@gmail.com>
2020-07-29 19:35:00 +0700

    Update html.js

--- src/html.js
+++ src/html.js
@@ -21,10 +21,6 @@ export default function HTML(props) {
         </noscript>
         <div key="body" id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
         {props.postBodyComponents}
-
-        <div id="comm100-button-0e5a654b-38bf-4f26-b8cf-73ca080596c6"></div>
-        <script type="text/javascript">
-          var Comm100API=Comm100API||{};(function(t){function e(e){var a=document.createElement("script"),c=document.getElementsByTagName("script")[0];a.type="text/javascript",a.async=!0,a.src=e+t.site_id,c.parentNode.insertBefore(a,c)}t.chat_buttons=t.chat_buttons||[],t.chat_buttons.push({code_plan:"0e5a654b-38bf-4f26-b8cf-73ca080596c6",div_id:"comm100-button-0e5a654b-38bf-4f26-b8cf-73ca080596c6"}),t.site_id=40000072,t.main_code_plan="0e5a654b-38bf-4f26-b8cf-73ca080596c6",e("https://vue.comm100.com/livechat.ashx?siteId="),setTimeout(function(){t.loaded||e("https://standby.comm100vue.com/livechat.ashx?siteId=")},5e3)})(Comm100API||{})
         </script>
       </body>
     </html>
